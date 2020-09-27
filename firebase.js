75bb444114afb6edeae45156b6926c576017b7e4
Premti <premti@gmail.com>
2020-09-27 10:52:00 +0100

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQq2uFoSesFvNVeLq-0BImQ8-QSJeIkCw",
+    authDomain: "tik-tok-sim-abda0.firebaseapp.com",
+    databaseURL: "https://tik-tok-sim-abda0.firebaseio.com",
+    projectId: "tik-tok-sim-abda0",
+    storageBucket: "tik-tok-sim-abda0.appspot.com",
+    messagingSenderId: "480339428102",
+    appId: "1:480339428102:web:203730b07e81ef50369487",
+    measurementId: "G-1C217RSYDB"
+  };
