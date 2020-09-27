b6534a7a07b192ee3a79ff766b14e66a3c3ec119
Premti <premti@gmail.com>
2020-09-27 15:08:12 +0100

    video can now play by clicking on it

--- src/Video.js
+++ src/Video.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Video.css'
 
 function Video(){
+
+    const videoRef = useRef(null);
+
+    const onVideoPress = () => {
+        videoRef.current.play()
+    }
+
     return (
         <div className="video">
-            <video src=""></video>
+            <video className="video_player" 
+            loop
+            onClick={onVideoPress}
+            ref={videoRef}
+            src="https://www.tiktok.com/@justamum/video/6876677387870915841">
+
+            </video>
         </div>
     )
 }
+
 
 export default Video
