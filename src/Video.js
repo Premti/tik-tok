11330a385a0d9da00b358bf8c078ee376e64e0a0
Premti <premti@gmail.com>
2020-09-29 13:01:11 +0100

    added an image of a record, and applied styling for it to spin

--- src/Video.js
+++ src/Video.js
@@ -25,7 +25,7 @@ function Video(){
             loop
             onClick={onVideoPress}
             ref={videoRef}
-            src="https://www.tiktok.com/@justamum/video/6876677387870915841">
+            src="https://v16-web-newkey.tiktokcdn.com/9c015008dcfc8f8f3647b591710e6b98/5f7253fd/video/tos/useast2a/tos-useast2a-pve-0068/225c730864614ba38b2b8b595ac5a427/?a=1988&br=2152&bt=1076&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020092815215301018907107022053167&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M3VnNXF3cnhmdjMzNDczM0ApOjo1aWloZGRnNzRkaDc0Z2cwZy8xNjVuM25fLS0wMTZzcy40NTU0YDE0Ml8tMDQzXjE6Yw%3D%3D&vl=&vr=">
 
             </video>
             <VideoFooter />
--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -6,7 +6,16 @@ function VideoFooter(){
         <div>
             <h2 className="videoFooter">
                 I am a footer
+                <div className="videoFooter_text">
+                    <h3>@Premti</h3>
+                    <p>This is a description</p>
+                </div>
+                <img className="videoFooter_record" src="https://static.thenounproject.com/png/934821-200.png" alt="">
+
+                </img>
             </h2>
         </div>
     )
 }
+
+export default VideoFooter
